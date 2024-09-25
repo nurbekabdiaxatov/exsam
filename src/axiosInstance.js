7f09ca32f78602dec95e7f2c95b959476362adb1
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-09-25 18:38:37 +0500

    MAIN
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://json-api.uz/api/project/exsam', // Adjust to your API base URL
-    timeout: 10000, // Optional: Set a timeout for requests
+    baseURL: 'https://json-api.uz/api/project/exsam', 
+    timeout: 10000, 
 });
 
 export default axiosInstance;
